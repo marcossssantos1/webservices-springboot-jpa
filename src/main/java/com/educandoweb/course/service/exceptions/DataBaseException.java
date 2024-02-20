6e3df1c47263a9b5e5ae0df6130fc4aa319e87df
Marcos Santos <marcossantosmsms604@gmail.com>
2024-02-20 20:57:57 -0300

    Exception handling - delete

---
+++ src/main/java/com/educandoweb/course/service/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.service.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
